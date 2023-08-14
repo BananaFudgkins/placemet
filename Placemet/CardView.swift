231fac3547e7e9bd676c954f5e84172d46a32a6b
Deja Jackson <jacksodl@lafayette.edu>
2023-08-14 09:42:39 -0700

    Working on card UI

--- Placemet/CardView.swift
+++ Placemet/CardView.swift
@@ -6,21 +6,42 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct CardView: View {
     let business: Business
     
     var body: some View {
         GeometryReader { geo in
-            HStack {
-                
+            VStack {
+                Image(systemName: "photo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: geo.size.width)
+                    .clipped()
+                    .padding()
+                HStack {
+                    if let name = business.name {
+                        Text(name)
+                    }
+                    Spacer()
+                    if let distance = business.distance {
+                        Text("\(distance) meters away")
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .padding()
             }
+            .background(Color(uiColor: .systemBackground))
+            .cornerRadius(12)
+            .shadow(radius: 4)
         }
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView(business: Business())
+        CardView(business: Business(name: "Fun Town", distance: 420))
     }
 }
