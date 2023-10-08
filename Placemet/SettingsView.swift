cbd245c330f00623b0f3f4cc0e01161edde92784
Deja Jackson <bananafudgkins@gmail.com>
2023-10-08 15:07:15 -0700

    Added options for designated contacts and a search radius slider

--- Placemet/SettingsView.swift
+++ Placemet/SettingsView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var searchRadius: Double = 700
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                Button("Add a designated contact", systemImage: "person.crop.circle.fill.badge.plus") {
+                    print("DO SOMETHING TWITTER BE LIKE")
+                }
+            } header: {
+                Text("Designated Contacts")
+            } footer: {
+                Text("You can send designated contacts a text message about where you're going before you go to meet someone you've matched with.")
+            }
+            Section {
+                VStack(spacing: 10) {
+                    Slider(value: $searchRadius, in: 0...40000, step: 5) {
+                        Text("Search radius")
+                    } minimumValueLabel: {
+                        Text("0 meters")
+                    } maximumValueLabel: {
+                        Text("40,000 meters")
+                    }
+                    Text(searchRadius == 1 ? "1 meter" : "\(searchRadius) meters")
+                }
+            } header: {
+                Text("Search radius")
+            }
+        }
     }
 }
 
